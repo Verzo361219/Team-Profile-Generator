f86275efddca12dd9a26bcc886f1c5c85a7b8c4b
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-09-21 20:45:34 -0400

    Fixed some bugs in the test scripts to run correctly and pass

--- team-profile-generator/test/Manager.test.js
+++ team-profile-generator/test/Manager.test.js
@@ -3,14 +3,14 @@ const Manager = require('../lib/Manager');
 
 // creating manager object  
 test('creates an Manager object', () => {
-    const manager = new Manager('Nicole', 90, 'nicole.elisaw@gmail', 4);
+    const manager = new Manager('Michael', 90, 'michael.hobbs@gmail', 4);
     
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 // gets role from getRole()
 test('gets role of employee', () => {
-    const manager = new Manager('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const manager = new Manager('Michael', 90, 'michael.hobbs@gmail.com');
 
     expect(manager.getRole()).toEqual("Manager");
 }); 
